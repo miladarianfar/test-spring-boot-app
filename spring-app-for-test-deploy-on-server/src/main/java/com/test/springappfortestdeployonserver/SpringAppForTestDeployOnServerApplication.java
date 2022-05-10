6832ee057c9a6b770186c9739e5eca1477ddd279
miladarianfar <68566120+miladarianfar@users.noreply.github.com>
2022-05-10 19:27:22 +0430

    Add files via upload

---
+++ spring-app-for-test-deploy-on-server/src/main/java/com/test/springappfortestdeployonserver/SpringAppForTestDeployOnServerApplication.java
@@ -0,0 +1,13 @@
+package com.test.springappfortestdeployonserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppForTestDeployOnServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAppForTestDeployOnServerApplication.class, args);
+    }
+
+}
---
+++ spring-app-for-test-deploy-on-server/src/main/java/com/test/springappfortestdeployonserver/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.test.springappfortestdeployonserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test/api")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String testApi() {
+        return "its just test api";
+    }
+}
